a8f2e6f1a416cb916143af8ec041f0a5be49fc79
jorge sierra <jorgemaikelsierra@outlook.com>
2023-04-11 20:13:36 -0500

    Actualizacion del plugin wordpress-seo

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite701a5e44ca56de0426dceec73ef8b0e::getLoader();
+return ComposerAutoloaderInit9e63524553be83976cae6f04e87c35fd::getLoader();
